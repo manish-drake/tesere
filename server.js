b4d2df6caef788e48ea47f9d49c0a3e9d835620b
Manish Verma <manish@drake.in>
2016-08-26 16:43:51 +0530

    CORS handler in server

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static('public'));
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 
 app.get('/run_test_get', function (req, res) {
